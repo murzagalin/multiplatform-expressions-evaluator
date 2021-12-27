9a22a9dddaf646e5f36e2c715205a778fbc1a27b
MurzagalinAR <azamat.em@gmail.com>
2021-12-27 11:14:26 +0100

    cleanup. removed unused fields

--- src/commonMain/kotlin/com/github/murzagalin/evaluator/Token.kt
+++ src/commonMain/kotlin/com/github/murzagalin/evaluator/Token.kt
@@ -17,53 +17,36 @@ sealed interface Token {
         value class Variable(val value: String) : Operand
     }
 
-    sealed class Operator(val priority: Int, val associativity: Associativity) : Token {
-        object Plus : Operator(6, Associativity.LEFT)
-        object Minus : Operator(6, Associativity.LEFT)
-        object Multiplication : Operator(7, Associativity.LEFT)
-        object Division : Operator(7, Associativity.LEFT)
-        object Modulo : Operator(7, Associativity.LEFT)
-        object Power : Operator(9, Associativity.RIGHT)
-        object UnaryMinus : Operator(8, Associativity.RIGHT)
-        object UnaryPlus : Operator(8, Associativity.RIGHT)
+    sealed interface Operator : Token {
+        object Plus : Operator
+        object Minus : Operator
+        object Multiplication : Operator
+        object Division : Operator
+        object Modulo : Operator
+        object Power : Operator
+        object UnaryMinus : Operator
+        object UnaryPlus : Operator
 
-        object And : Operator(3, Associativity.LEFT)
-        object Or : Operator(2, Associativity.LEFT)
-        object Not : Operator(4, Associativity.LEFT)
+        object And : Operator
+        object Or : Operator
+        object Not : Operator
 
-        object GreaterThan : Operator(5, Associativity.LEFT) //>
-        object GreaterEqualThan : Operator(5, Associativity.LEFT) //>=
-        object LessThan : Operator(5, Associativity.LEFT) //<
-        object LessEqualThan : Operator(5, Associativity.LEFT) //<=
-        object Equal : Operator(5, Associativity.LEFT) //==
-        object NotEqual : Operator(5, Associativity.LEFT) //!=
+        object GreaterThan : Operator //>
+        object GreaterEqualThan : Operator //>=
+        object LessThan : Operator //<
+        object LessEqualThan : Operator //<=
+        object Equal : Operator //==
+        object NotEqual : Operator //!=
 
-        object TernaryIf : Operator(1, Associativity.RIGHT)
-        object TernaryElse : Operator(1, Associativity.LEFT)
-        object TernaryIfElse : Operator(1, Associativity.RIGHT)
+        object TernaryIf : Operator
+        object TernaryElse : Operator
+        object TernaryIfElse : Operator
     }
 
     data class FunctionCall(
         val argsCount: Int,
         val function: Function
     ) : Token {
-
-        operator fun invoke(args: List<Operand>): Operand {
-            val functionArgs = mutableListOf<Any>()
-            args.forEach {
-                when (it) {
-                    is Operand.Number -> functionArgs.add(it.value)
-                    is Operand.Boolean -> functionArgs.add(it.value)
-                    else -> error("operand type ${it::class.simpleName} is not supported in function calls")
-                }
-            }
-
-            return when (val result = function(*functionArgs.toTypedArray())) {
-                is Number -> Operand.Number(result.toDouble())
-                is Boolean -> Operand.Boolean(result)
-                else -> error("function return type ${result::class.simpleName} is not supported")
-            }
-        }
 
         operator fun invoke(args: List<Any>) = function(*args.toTypedArray())
 
@@ -74,6 +57,4 @@ sealed interface Token {
         object Left : Bracket()
         object Right : Bracket()
     }
-
-    enum class Associativity { LEFT, RIGHT }
 }
